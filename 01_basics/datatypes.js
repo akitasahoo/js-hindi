a665f93bcd131ea207ec80fa8b46b144a6ee10a8
Ankita Sahoo <akitasahoo@gmail.com>
2026-06-02 14:28:48 +0000

    stack and heap

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -44,3 +44,32 @@ console.log(typeof heros);//object
 console.log(typeof myObj);
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//++++++++++++++++++++++++++++++++
+
+//Stack (primitive) and Heap (non primitive) memory
+
+//Primitive data types are stored in stack memory and non primitive data types are stored in heap memory.
+
+//In stack memory, the actual value is stored and in heap memory, a reference to the value is stored.
+
+//When we assign a primitive value to a variable, the value is stored in the stack memory. When we assign a non primitive value to a variable, a reference to the value is stored in the stack memory and the actual value is stored in the heap memory.
+
+let myName = "Ankita"
+let anotherName = myName
+anotherName="Ankita Sahoo"
+console.log(myName);//Ankita
+console.log(anotherName);//Ankita Sahoo
+
+let userOne = {
+    email: "ueer@google.com", 
+    upi: "user@ybl"
+}
+
+let userTwo=userOne
+userTwo.email="userTwo@google.com"
+
+console.log(userOne.email);//ueer@google.com
+console.log(userTwo.email);//userTwo@google.com
