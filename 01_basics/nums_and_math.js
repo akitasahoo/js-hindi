4eb71f208d83f0f2859f2354d4b564fd4b805b62
Ankita Sahoo <akitasahoo@gmail.com>
2026-06-06 08:20:51 +0000

    min and max

--- 01_basics/nums_and_math.js
+++ 01_basics/nums_and_math.js
@@ -14,3 +14,15 @@ console.log(otheNumber.toPrecision(4));
 
 const hundred = 10000000
 console.log(hundred.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+
+console.log(Math.min(4,5,7,6));
+console.log(Math.max(4,5,7,6));
+
+console.log(Math.random());
+console.log(Math.random() * 10+1);
